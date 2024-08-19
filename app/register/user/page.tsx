6fe6ec56f91925a6e1ee7d5475bc4c48ab836d9b
strwbry <annisamustofa@outlook.com>
2024-08-19 16:09:28 +0700

    feat(SE-69 register): init WIP user registration form

---
+++ app/register/user/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegistrationForm from "@/app/register/user/RegistrationForm";
+
+const UserRegister: React.FC = () => {
+  return (
+    <div>
+      User Register here
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default UserRegister;
