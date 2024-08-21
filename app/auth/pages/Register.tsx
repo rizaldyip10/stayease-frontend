d40b89e1d8e3fd5dfa88bd6dceea31701f1d6996
strwbry <annisamustofa@outlook.com>
2024-08-21 14:35:20 +0700

    feat(SE-69 register): Init WIP log-in page and added AuthLayout

---
+++ app/auth/pages/Register.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import FormSection from "@/app/auth/components/FormSection";
+import SidePicture from "@/app/auth/components/SidePicture";
+
+const Register: React.FC = () => {
+  return (
+    <>
+      <FormSection className="form mb-2 w-full h-full md:w-2/5" />
+      <SidePicture
+        alt="sign-up"
+        src="/sign-up-side-pic.webp"
+        className="hidden md:flex"
+      />
+    </>
+  );
+};
+
+export default Register;
