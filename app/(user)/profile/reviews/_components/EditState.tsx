431ce81d2af909b7fb2735508fb4ad35f064ec26
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-08-30 14:37:20 +0700

    feature: user reviews card (#3)
    * feat: user layout and booking page structure
    
    * feat(booking page): complete layout from mobile to desktop. TO DO: connect to backend
    
    * feat: value to send structure
    
    * feat: adding api endpoint to handle booking
    
    * feat: user profile page folder structure plus admin dashboard layout and folder structure
    
    * feat: user review card and profile mobile menu

---
+++ app/(user)/profile/reviews/_components/EditState.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import * as yup from "yup";
+import {FC} from "react";
+import {whiteSpaceRegex} from "@/constants/WhiteSpaceRegex";
+import {Form, Formik, FormikValues} from "formik";
+import {Button} from "@/components/ui/button";
+import StarInput from "@/components/formik/StarInput";
+import FormikInput from "@/components/formik/FormikInput";
+
+interface EditStateProps {
+    rating?: number;
+    comment?: string;
+}
+
+const EditState: FC<EditStateProps> = ({ rating, comment }) => {
+    const reviewSchema = yup.object().shape({
+        rating: yup.number().min(1, "Unable to give rating below 1").max(5, "Unable to give rating above 5").required("Rating required"),
+        comment: yup.string().min(3, "Min 3 characters").matches(whiteSpaceRegex, "Please enter valid review").required(),
+    });
+
+    const initialValue = {
+        rating: rating || 0,
+        comment: comment || "",
+    }
+
+    const handleEditReview = async (value: FormikValues) => {
+        try {
+            console.log(value)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <Formik
+            initialValues={initialValue}
+            validationSchema={reviewSchema}
+            onSubmit={async (value) => {
+                await handleEditReview(value);
+            }}
+        >
+            {
+                () => (
+                    <Form className="w-full grid grid-cols-1 gap-y-2">
+                        <StarInput />
+                        <FormikInput name="comment" label="How's your stay?" />
+                        <Button
+                            variant="link"
+                            type="submit"
+                            className="w-max p-0"
+                        >
+                            Submit
+                        </Button>
+                    </Form>
+                )
+            }
+        </Formik>
+    );
+};
+
+export default EditState;
--- app/(user)/profile/reviews/page.tsx
+++ app/(user)/profile/reviews/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import ReviewList from "@/app/(user)/profile/reviews/_components/ReviewList";
+
 
 const UserReviewPage = () => {
     return (
-        <div>
-            User Review Page
+        <div className="w-full flex flex-col gap-5">
+            <h1 className="text-lg text-blue-950 font-semibold">My Reviews</h1>
+            <ReviewList />
         </div>
     );
 };
