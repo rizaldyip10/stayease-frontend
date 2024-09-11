175b298dcf77a58271669afa1914a769587c7d55
strwbry <annisamustofa@outlook.com>
2024-09-11 14:11:14 +0700

    Refactor(SE-59): Added a few packages

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
 import HeroSection from "@/app/(user)/_components/hero-section/HeroSection";
-import PropertySearchBar from "@/components/PropertySearchBar";
-import LandingPageListings from "@/app/(user)/_components/landing-page-listing/LandingPageListings";
 
 export default function Home() {
   return (
     <main className="">
       <HeroSection className="flex flex-col items-center justify-between md:p-6 md:min-h-[695px] md:max-h-[695px] max-h-screen" />
-      <LandingPageListings />
+      {/*<LandingPageListings />*/}
     </main>
   );
 }
