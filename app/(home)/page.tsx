8760e0c45ce7f9f9f72972eb0b84093cbe5558e8
chocological13 <155338364+chocological13@users.noreply.github.com>
2024-10-11 00:13:47 +0700

    feat(docker): improve NEXT_PUBLIC env var handling in Dockerfile (#39)
    - Update Dockerfile to set NEXT_PUBLIC environment variables
    - Modify next.config.mjs to explicitly use NEXT_PUBLIC variables
    - Add logging for environment variables in (home)/page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -30,3 +30,23 @@ export default function Home() {
     </main>
   );
 }
+
+// ! only for debugging, delete when done
+if (typeof window === "undefined") {
+  console.log("Server-side Environment Variables:");
+  console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+  console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
+  console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
+  console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
+  console.log("Server-side NEXT_PUBLIC environment check:");
+  console.log("NEXT_PUBLIC_BASE_URL:", process.env.NEXT_PUBLIC_BASE_URL);
+  console.log("NEXT_PUBLIC_BACKEND_URL:", process.env.NEXT_PUBLIC_BACKEND_URL);
+  console.log(
+    "NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:",
+    process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  );
+  console.log(
+    "NEXT_PUBLIC_GOOGLE_MAPS_ID:",
+    process.env.NEXT_PUBLIC_GOOGLE_MAPS_ID,
+  );
+}
