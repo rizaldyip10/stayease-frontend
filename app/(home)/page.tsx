3e54c000073e4362d9aeefcabbf6badd69572db4
strwbry <annisamustofa@outlook.com>
2024-10-10 22:45:30 +0700

    fix: add env var logging to debug cloud run deployment and explicitly pass env var in `next.config.mjs`

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,27 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import LandingPage from "@/app/(home)/_components/landing-page/LandingPage";
 import PageWrapper from "@/components/PageWrapper";
 
 export default function Home() {
+  // ! only for logging, delete when done
+  useEffect(() => {
+    console.log("Environment Variables:");
+    console.log("NEXT_PUBLIC_BASE_URL:", process.env.NEXT_PUBLIC_BASE_URL);
+    console.log(
+      "NEXT_PUBLIC_BACKEND_URL:",
+      process.env.NEXT_PUBLIC_BACKEND_URL,
+    );
+    console.log(
+      "NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:",
+      process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    );
+    console.log(
+      "NEXT_PUBLIC_GOOGLE_MAPS_ID:",
+      process.env.NEXT_PUBLIC_GOOGLE_MAPS_ID,
+    );
+  }, []);
+
   return (
     <main>
       <PageWrapper className="">
